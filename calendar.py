ba47a0cf795a7f6e15dacc777b7596715a78161b
Coutj <coutinholima.j@gmail.com>
2019-07-12 16:55:36 -0300

    mes obtido corretamente

--- calendar.py
+++ calendar.py
@@ -1,4 +1,20 @@
-def main():
-    day;
-    month;
-    year;
+from datetime import date
+
+months = ["Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", 
+          "Setembro", "Outubro", "Novembro", "Dezembro"]
+
+def GetMMYYYY (tipo):
+    
+    tipo = tipo.upper()
+    
+    if (tipo.upper() == "MM"):
+        actualMonth = date.today().strftime('%m')
+        print (months [int(actualMonth)])
+    else:
+        print("Errou")
+
+
+
+print (type(date.today()))
+GetMMYYYY("mm");
+
